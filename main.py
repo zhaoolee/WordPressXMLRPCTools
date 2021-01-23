55aaa1890a65ad202a3f00bb70165786012856ad
zhaoolee <zhaoolee@gmail.com>
2021-01-23 22:20:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def post_link_id_list_2_link_id_dic(post_link_id_list):
 
 
 def href_info(link):
-    return "<br/><br/><br/>\n\n\n---\n## 本文永久更新地址: \n[" + link + "](" + link + ")"
+    return "<br/><br/><br/>\n\n\n\n## 本文永久更新地址: \n[" + link + "](" + link + ")"
 
 # 在README.md中插入信息文章索引信息，更容易获取google的收录
 def insert_index_info_in_readme():
