68a65157763ab9a938e0a69fdf0dc29dfa237b5e
Chengwei Ye <cye3@wpi.edu>
2022-06-19 17:25:32 -0400

    update main.py

--- main.py
+++ main.py
@@ -235,8 +235,10 @@ def main():
     post_link_id_list = get_posts()
     print(post_link_id_list)
     link_id_dic = post_link_id_list_2_link_id_dic(post_link_id_list)
+    link_id_dic = {key.split('/')[-2]: value for (key,value) in link_id_dic.items()}
     print(link_id_dic)
-    wp_links = [link.split('/')[-1] for link in link_id_dic.keys()]
+    # wp_links = [link.split('/')[-1] for link in link_id_dic.keys()]
+
     # 2. 获取md_sha1_dic
     # 查看目录下是否存在md_sha1.txt,如果存在则读取内容；
     # 如果不存在则创建md_sha1.txt,内容初始化为{}，并读取其中的内容；
@@ -246,11 +248,10 @@ def main():
     # 3. 开始同步
     # 读取_posts目录中的md文件列表
     md_list = get_md_list(os.path.join(os.getcwd(), "_posts"))
-    md_list = [os.path.basename(md) for md in md_list]
 
     for md in md_list:
         # 计算md文件的sha1值，并与md_sha1_dic做对比
-        sha1_key =  md
+        sha1_key =  os.path.basename(md)
         sha1_value = get_sha1(md)
         # 如果sha1与md_sha1_dic中记录的相同，则打印：XX文件无需同步;
         if((sha1_key in md_sha1_dic.keys()) and (sha1_value == md_sha1_dic[sha1_key])):
@@ -267,22 +268,25 @@ def main():
             link = sha1_key.split(".")[0]
             content = markdown.markdown(content + href_info("https://"+domain_name+"/p/"+link+"/"), extensions=['tables', 'fenced_code'])
             # 如果文章id不存在,则直接新建
-            if link not in wp_links:
+            if link not in link_id_dic.keys():
                 print(f'Creating new post {md}')
                 new_post(title, content, link, post_status, terms_names_post_tag, terms_names_category)
             # 如果文章有id, 则更新文章
             else:
                 # 获取id
                 print(f'Updating existing post {md}')
-                id = link_id_dic["https://"+domain_name+"/p/"+link+"/"]
+                id = link_id_dic[link]
                 edit_post(id, title, content, link, post_status, terms_names_post_tag, terms_names_category)
 
     # 如果_posts中的markdown被删除，则删除对应的post
     for md in md_sha1_dic.keys():
-        if md not in md_list:
+        if md == 'update_time':
+            continue
+        md_list_basename = [os.path.basename(md) for md in md_list]
+        if md not in md_list_basename:
             print(f'Deleting post {md}')
             link = md.split(".")[0]
-            id = link_id_dic["https://"+domain_name+"/p/"+link+"/"]
+            id = link_id_dic[link]
             delete_post(id)
 
 
